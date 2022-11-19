f6e8bcd1c5e8c660171e57105f4817c480534afb
Leszczytom <leszczytom@gmail.com>
2022-11-19 15:51:56 +0000

    👷 Added two cloud run service.

--- main.tf
+++ main.tf
@@ -9,6 +9,46 @@ terraform {
 }
 
 provider "google" {
-    project = " ceri-m1-ecommerce-2022"
+    project = "ceri-m1-ecommerce-2022"
     region  = "europe-west1"
+}
+
+resource "google_cloud_run_service" "default" {
+  name     = "CLOUD_RUN_BACKEND"
+  location = "europe-west1"
+
+  metadata {
+    annotations = {
+      "autoscaling.knative.dev/maxScale" = 1
+      "autoscaling.knative.dev/minScale" = 1
+    }
+  }
+
+  template {
+    spec {
+      containers {
+        image = "europe-west1-docker.pkg.dev/ceri-m1-ecommerce-2022/pinkzebra/backend"
+      }
+    }
+  }
+}
+
+resource "google_cloud_run_service" "default" {
+  name     = "CLOUD_RUN_FRONTEND"
+  location = "europe-west1"
+
+  metadata {
+    annotations = {
+      "autoscaling.knative.dev/maxScale" = 1
+      "autoscaling.knative.dev/minScale" = 1
+    }
+  }
+  
+  template {
+    spec {
+      containers {
+        image = "europe-west1-docker.pkg.dev/ceri-m1-ecommerce-2022/pinkzebra/frontend"
+      }
+    }
+  }
 }
