cb59d2af521b4f36f14d92a86ecaff92af22d6fb
Leszczytom <leszczytom@gmail.com>
2022-12-12 15:09:11 +0000

    Update CI

--- main.tf
+++ main.tf
@@ -8,6 +8,10 @@ terraform {
   }
 }
 
+data "google_secret_manager_secret" "address" {
+  secret_id = "mysql/address"
+}
+
 provider "google" {
   project = "ceri-m1-ecommerce-2022"
   region  = "europe-west1"
@@ -22,6 +26,15 @@ resource "google_cloud_run_service" "back" {
       service_account_name = "terraform-pinkzebra@ceri-m1-ecommerce-2022.iam.gserviceaccount.com"
       containers {
         image = "europe-west1-docker.pkg.dev/ceri-m1-ecommerce-2022/pinkzebra/backend:latest"
+        env {
+          name = "DATABASE_ADDRESS"
+          value_from {
+            secret_key_ref {
+              name = "mysql-address" # data.google_secret_manager_secret.address.secret_id
+              key  = "latest"
+            }
+          }
+        }
       }
     }
   }
