173637cc6c4a9147579b2b2d86ee652a1956252d
Tom Leszczynski <37774352+LeszczyTom@users.noreply.github.com>
2022-12-12 11:21:56 +0100

    Update main.tf
    - env

--- main.tf
+++ main.tf
@@ -10,7 +10,6 @@ terraform {
 
 data "google_secret_manager_secret" "address" {
   secret_id = "mysql.address"
-  version   = "1"
 }
 
 provider "google" {
@@ -32,15 +31,6 @@ resource "google_cloud_run_service" "back" {
     spec {
       service_account_name = "terraform-pinkzebra@ceri-m1-ecommerce-2022.iam.gserviceaccount.com"
       containers {
-        env {
-          name = "DATABASE_ADDRESS"
-          value_from {
-            secret_key_ref {
-              name = data.google_secret_manager_secret.address.secret_id
-              key  = "latest"
-            }
-          }
-        }
         image = "europe-west1-docker.pkg.dev/ceri-m1-ecommerce-2022/pinkzebra/backend:latest"
       }
     }
