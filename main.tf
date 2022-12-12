dadffb5f81bdea6fb79550d76bb432b5b6a28f27
Tom Leszczynski <37774352+LeszczyTom@users.noreply.github.com>
2022-12-12 10:46:38 +0100

    Update main.tf
    Added env variables for backend.
    Added service_account_name for each resource

--- main.tf
+++ main.tf
@@ -8,6 +8,10 @@ terraform {
   }
 }
 
+data "google_secret_manager_secret" "address" {
+  secret_id = "mysql.address"
+}
+
 provider "google" {
   project = "ceri-m1-ecommerce-2022"
   region  = "europe-west1"
@@ -25,7 +29,17 @@ resource "google_cloud_run_service" "back" {
 
   template {
     spec {
+      service_account_name = "terraform-pinkzebra@ceri-m1-ecommerce-2022.iam.gserviceaccount.com"
       containers {
+        env {
+          name = "DATABASE_ADDRESS"
+          value_from {
+            secret_key_ref {
+              name = data.google_secret_manager_secret.address.secret_id
+              key = "latest"
+            }
+          }
+        }
         image = "europe-west1-docker.pkg.dev/ceri-m1-ecommerce-2022/pinkzebra/backend:latest"
       }
     }
@@ -44,6 +58,7 @@ resource "google_cloud_run_service" "front" {
 
   template {
     spec {
+      service_account_name = "terraform-pinkzebra@ceri-m1-ecommerce-2022.iam.gserviceaccount.com"
       containers {
         image = "europe-west1-docker.pkg.dev/ceri-m1-ecommerce-2022/pinkzebra/frontend:latest"
       }
